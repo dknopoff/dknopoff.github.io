31347c9967614c0174e79e995f295af7c7de91cb
Dustin <dustinknopoff@gmail.com>
2018-04-16 08:10:27 -0400

    github logo added

--- src/stache.py
+++ src/stache.py
@@ -9,9 +9,9 @@ def writing(path, name):
     with open(path, 'r', encoding='utf8') as f:
         j = json.load(f)
         j['title'] = name
-        with open('/Users/Dustin/Documents/Gits/Python/portfolio/src/templates/index.mustache', 'r') as g:
+        with open('/Users/Dustin/Documents/Gits/Web/dustinknopoff.github.io/src/templates/index.mustache', 'r') as g:
             data = g.read()
-            with open('/Users/Dustin/Documents/Gits/Python/portfolio/out/' + name + '.html', 'w+',
+            with open('/Users/Dustin/Documents/Gits/Web/dustinknopoff.github.io/out/' + name + '.html', 'w+',
                       encoding='utf8') as out:
                 out.write(pystache.render(data, j))
 
